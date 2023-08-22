e87c326bdc5f58cdd8ec8d9727ced478f3a48da8
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-08-21 21:44:11 -0400

    fixed modifiedSentence

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/SearchString.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/SearchString.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 public class SearchString {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a search term: ");
+        String searchTerm = input.nextLine();
 
-        String alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
+        String firstSentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
 
-        System.out.println("Enter a term to search for within the string: ");
-        String chunk = input.nextLine();
-
-        System.out.println(alice.indexOf(chunk));
+        int index = firstSentence.indexOf(searchTerm);
+        int length = searchTerm.length();
+        System.out.println("Your search term first appears at index " + index + ". Your term is " + length + " characters long.");
+        String modifiedSentence = firstSentence.replace(searchTerm, "");
+        System.out.println(modifiedSentence);
     }
 }
