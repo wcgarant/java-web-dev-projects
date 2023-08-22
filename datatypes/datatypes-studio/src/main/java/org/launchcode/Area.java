10ab9a29b0129e0a8587fb721d58294a65b4e52e
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-08-21 20:52:42 -0400

    completed studio

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,18 @@
+package org.launchcode;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class Area {
+   public static void main(String[] args) {
+
+      Scanner input = new Scanner(System.in);
+
+      System.out.println("Enter a radius:");
+
+      double radius = input.nextDouble();
+
+      double area = Circle.getArea(radius);
+
+      System.out.println("The area of a circle of radius " + radius + " is: " + area);
+   }
+}
