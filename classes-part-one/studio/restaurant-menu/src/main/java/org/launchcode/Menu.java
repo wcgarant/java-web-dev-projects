8e2b9136d6cda5c57fbf9b2fc7e0f10327ccd0af
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-08-28 21:23:41 -0400

    finished studio

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,30 +1,9 @@
-package org.launchcode;
-
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Menu {
-    private Date lastUpdated;
-    private ArrayList<MenuItem> items;
+    private ArrayList<MenuItem> menu = new ArrayList<>();
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
-        this.lastUpdated = d;
-        this.items = i;
-    }
-
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public void setItems(ArrayList<MenuItem> items) {
-        this.items = items;
-    }
-
-    public Date getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public ArrayList<MenuItem> getItems() {
-        return items;
+    public ArrayList<MenuItem> getMenu() {
+        return menu;
     }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,31 +1,37 @@
-package org.launchcode;
+import javax.xml.namespace.QName;
 
 public class MenuItem {
+    private String name;
     private double price;
     private String description;
     private String category;
-    private boolean isNew;
+    private boolean newItem;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
-        this.price = p;
-        this.description = d;
-        this.category = c;
-        this.isNew = iN;
-    }
-
-    public void setPrice(double price) {
+    public MenuItem(String name, double price, String description, String category, boolean newItem) {
+        this.name = name;
         this.price = price;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
-    }
-
-    public void setCategory(String category) {
         this.category = category;
+        this.newItem = newItem;
     }
 
-    public void setNew(boolean aNew) {
-        isNew = aNew;
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isNewItem() {
+        return newItem;
     }
 }
